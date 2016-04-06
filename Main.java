f9e5cbc7b6f96bbf6e27570cf6b9ff571144de44
gibrown1 <gibrown1@catamount.wcu.edu>
2016-04-06 13:49:36 -0400

    Added files via upload
    Added switchUI method in main to facilitate switching screen, changed name of screen_1a file and added scroll panes.

---
+++ Main.java
@@ -0,0 +1,74 @@
+package application;
+
+import Screen.ScreenInterface;
+import Screen.Screen_1;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+
+public class Main extends Application {
+	private Pane root;
+
+	private Scene scene;
+
+	enum Screens {SCREEN_1, SCREEN_1A,SCREEN_2,SCREEN_2A, SCREEN_2A1,SCREEN_2B,SCREEN_2B1,SCREEN_2B2,
+					SCREEN_2B3,SCREEN_2C,SCREEN_2D,SCREEN_2D1};
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			switchUI(Screens.SCREEN_1);
+			scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	public void switchUI(Screens screen){
+		switch(screen){
+			case SCREEN_1:
+					root = (Pane) Screen_1.getInstance();
+					break;
+			case SCREEN_1A:
+				break;
+			case SCREEN_2:
+				break;
+			case SCREEN_2A:
+				break;
+			case SCREEN_2A1:
+				break;
+			case SCREEN_2B:
+				break;
+			case SCREEN_2B1:
+				break;
+			case SCREEN_2B2:
+				break;
+			case SCREEN_2B3:
+				break;
+			case SCREEN_2C:
+				break;
+			case SCREEN_2D:
+				break;
+			case SCREEN_2D1:
+				break;
+			default:
+				break;
+		}
+
+		if(scene == null){
+			scene = new Scene(root,400,400);
+		}
+		else{
+			scene.setRoot(root);
+		}
+	}
+}
---
+++ Screen_1.java
@@ -0,0 +1,124 @@
+package Screen;
+
+import java.util.ArrayList;
+
+import javafx.collections.ObservableList;
+import javafx.geometry.HPos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+
+public class Screen_1 extends GridPane implements ScreenInterface{
+
+	private static Screen_1 instance;
+
+	private Screen_1() {
+		setConstraints();
+		makeComponents();
+	}
+
+	/**
+	 * The getInstance method returns the singleton instance of the screen
+	 *  @return instance
+	 */
+	public static ScreenInterface getInstance(){
+		if(instance != null){
+			return instance;
+		}
+		else{
+			instance = new Screen_1();
+			return instance;
+		}
+	}
+
+	/**
+	 * This method is a helper to the constructor of the class to make itself
+	 * without having an extremely long constructor
+	 *
+	 */
+	private void setConstraints(){
+		int columnNumber = 3;
+		int rowNumber = 6;
+		for(int i = 0; i < columnNumber;i++){
+			ColumnConstraints col0 = new ColumnConstraints();
+			col0.setPercentWidth(40);
+			if(i == 1){
+				col0.setPercentWidth(20);
+			}//end if
+			this.getColumnConstraints().add(col0);
+		}//end for
+
+		for(int i = 0; i<rowNumber;i++){
+			RowConstraints row0 = new RowConstraints();
+			row0.setPercentHeight(10);
+			if(i == 1){//if its the message row
+				row0.setPercentHeight(5);
+			}
+			else if(i == 3){//the list's row
+				row0.setPercentHeight(50);
+			}
+			else if(i == 4){//the browse button
+				row0.setPercentHeight(15);
+			}
+			this.getRowConstraints().add(row0);
+		}//end for
+	}//end setConstraints
+
+
+	/**
+	 * This method defines the components on the screen and adds them to it.
+	 */
+	private void makeComponents(){
+		Label vName = new Label("Venue Name");
+		Label message = new Label("Message");
+
+		this.add(vName,1,0,1,1);
+		this.add(message,1,1,1,1);
+
+
+		Label pop = new Label("Popular Songs");
+		Label newSongs = new Label("New Songs");
+
+		this.add(pop,0,2,1,1);
+		this.add(newSongs,2,2,1,1);
+
+		//The list stuff will go here, empty for now
+
+		Button browse = new Button("Browse");
+		//Add listener for the button later
+		this.add(browse,1,4,1,1);
+
+		Label nowPlaying = new Label("Now Playing"); //make this its own component later
+		this.add(nowPlaying,1,5,1,1);
+
+		ScrollPane sc1 = new ScrollPane();
+		ScrollPane sc2 = new ScrollPane();
+
+		sc1.setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
+		sc1.setVbarPolicy(ScrollBarPolicy.ALWAYS);
+
+		sc2.setHbarPolicy(ScrollBarPolicy.AS_NEEDED);
+		sc2.setVbarPolicy(ScrollBarPolicy.ALWAYS);
+
+		this.add(sc1,0,3,1,1);
+
+
+		this.add(sc2,2,3,1,1);
+
+		setCenterAlignment();
+	}//end makeComponents
+
+
+	void setCenterAlignment(){
+		ObservableList<Node> elements = this.getChildren();
+		for(Node i:elements){
+			GridPane.setHalignment(i,HPos.CENTER);
+		}
+	}
+
+}
