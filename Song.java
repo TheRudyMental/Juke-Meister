5866479fcb39e3e12143820af5bb496715b2c3bf
jdburchette2 <jdburchette2@catamount.wcu.edu>
2016-04-29 23:14:04 -0400

    Added Comments

--- Song.java
+++ Song.java
@@ -4,20 +4,38 @@ import java.io.File;
 import java.sql.Timestamp;
 import java.util.Calendar;
 
-
+/**
+ * Holds all data related to a single song
+ * @author Jamie Burchette, Grant Brown, Zacharay Larenzo
+ * @version 4/29/16
+ */
 public class Song implements SongIF  {
 
+	//Info about song
 	private String title;
 	private String artist;
 	private int year;
 	private File songFile;
 	private File picture;
+	
+	//Id in table
 	private int id;
+	
+	//Counters for popularity
 	public int weekCounter;
 	private int monthCounter;
+	
+	//Time song was created
 	private Timestamp dateAdded;
 
-
+	/**
+	 * Initiates the song object
+	 * @param title title of song
+	 * @param artist artist who performed song
+	 * @param year year song was released
+	 * @param songFile File that contains song
+	 * @param picture image file to accompany song
+	 */
 	public Song(String title, String artist, int year, File songFile, File picture){
 		this.title = title;
 		this.artist = artist;
@@ -30,14 +48,34 @@ public class Song implements SongIF  {
 		dateAdded = new Timestamp(calendar.getTimeInMillis());
 	}
 
+	/**
+	 * Initiates the song object
+	 * @param title title of song
+	 * @param artist artist who performed song
+	 * @param songFile File that contains song
+	 * @param picture image file to accompany song
+	 */
 	protected Song(String title, String artist, File songFile, File picture){
 		this(title, artist, 0, songFile, picture);
 	}
 
+	/**
+	 * Initiates the song object
+	 * @param title title of song
+	 * @param artist artist who performed song
+	 * @param year year song was released
+	 * @param songFile File that contains song
+	 */
 	protected Song(String title, String artist, int year, File songFile){
 		this(title, artist, year, songFile, null);
 	}
 
+	/**
+	 * Initiates the song object
+	 * @param title title of song
+	 * @param artist artist who performed song
+	 * @param songFile File that contains song
+	 */
 	protected Song(String title, String artist, File songFile){
 		this(title, artist, 0, songFile, null);
 	}
@@ -127,22 +165,35 @@ public class Song implements SongIF  {
 		this.monthCounter = monthCounter;
 	}
 
+	/**
+	 * Increases counters when song is played
+	 */
 	@Override
 	public void addCount(){
 		weekCounter++;
 		monthCounter++;
 	}
 
+	/**
+	 * Allows resetting at end of week
+	 */
 	@Override
 	public void resetWeek(){
 		weekCounter = 0;
 	}
 
+	/**
+	 * Allows resetting at end of month
+	 */
 	@Override
 	public void resetMonth(){
 		monthCounter = 0;
 	}
 
+	/**
+	 * Checks if a title is the same as this song's title
+	 * @param title the title being checked
+	 */
 	@Override
 	public boolean equals(String title) {
 		if(title.equals(this.title)){
